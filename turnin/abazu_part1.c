97fa4a6110849662c76fb460570f14b2af84330f
Andrew Bazua <abazu001@wch136-19.cs.ucr.edu>
2019-11-04 13:02:00 -0800

    Initialize repo (Part 1 complete)

---
+++ turnin/abazu_part1.c
@@ -0,0 +1,48 @@
+/*	Author: abazu001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #8  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+    // ADEN: setting this bit eneables analog-to-digital conversion.
+    // ADSC: setting this bit starts the first conversion
+    // ADATE: setting this bit enables auto-triggering. SInce we are
+    //        in Free Running Mode, a new conversion will trigger whenever
+    //        the previous conversion completes.
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    ADC_init();
+
+    unsigned short potential = 0x00;
+    unsigned char tmpB = 0x00;
+    unsigned char tmpD = 0x00;
+        
+
+    /* Insert your solution below */
+    while (1) {
+        potential = ADC;
+        tmpB = (char)potential;
+        tmpD = (char)(potential >> 8);
+
+        PORTB = tmpB;
+        PORTD = tmpD;
+
+    }
+    return 1;
+}
